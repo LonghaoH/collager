c54f1ad0265df584ce0346f3b02b456f76b4fb05
JesseH44 <31856946+JesseH44@users.noreply.github.com>
2023-03-16 00:52:39 -0400

    Add files via upload

--- src/model/Layer.java
+++ src/model/Layer.java
@@ -10,6 +10,13 @@ public class Layer {
   private model.Filter filter;
   private PixelColor[][] layerImage;
 
+  /**
+   * Initializes the layer.
+   * @param name the name of the layer
+   * @param height the height of the layer
+   * @param width the width of the layer
+   * @param filter the filter applied to the layer
+   */
   public Layer(String name, int height, int width, Filter filter) {
     this.name = name;
     this.height = height;
@@ -18,6 +25,18 @@ public class Layer {
     this.layerImage = new PixelColor[this.height][this.width];
   }
 
+  /**
+   * Returns the name of the layer.
+   * @return the name of the layer
+   */
+  public String getName() {
+    return this.name;
+  }
+
+  /**
+   * Makes the layer fully white with no transparency.
+   * @return a fully white layer
+   */
   public Layer whiteLayer() {
     for (int i = 0; i < height; i++) {
       for (int k = 0; k < width; k++) {
@@ -27,6 +46,10 @@ public class Layer {
     return this;
   }
 
+  /**
+   * Makes the layer a default layer
+   * @return a white layer with full transparency
+   */
   public Layer defaultLayer() {
     for (int i = 0; i < height; i++) {
       for (int k = 0; k < width; k++) {
@@ -36,5 +59,12 @@ public class Layer {
     return this;
   }
 
-
+  /**
+   * Adds an image to the layer
+   * @param imageName the filepath of the image to be added
+   * @param xPos the x-position of the top-leftmost corner of the image
+   * @param yPos the y-position of the top-leftmost corner of the image
+   */
+  public void addImage(String imageName, int xPos, int yPos) {
+  }
 }
