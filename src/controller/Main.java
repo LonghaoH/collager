74cda79cf91ca98eecf687b919c21b42e55a8a44
JesseH44 <31856946+JesseH44@users.noreply.github.com>
2023-03-18 20:29:40 -0400

    Add files via upload

---
+++ src/controller/Main.java
@@ -0,0 +1,22 @@
+package controller;
+
+import java.io.IOException;
+
+/**
+ * A class to run CollagerPPM.
+ */
+public class Main {
+
+  /**
+   * Runs the collager program.
+   *
+   * @param args arguments
+   */
+  public static void main(String args[]) {
+    try {
+      new CollagerControllerImpl(System.out).run();
+    } catch (IOException e) {
+      throw new RuntimeException();
+    }
+  }
+}
