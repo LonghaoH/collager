890e647386b31c7e1a12fa57be004f4e7d0d2996
Nathan Huang <longhaoh1370@gmail.com>
2023-03-18 20:58:26 -0400

    Update Main.java

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -12,7 +12,7 @@ public class Main {
    *
    * @param args arguments
    */
-  public static void main(String args[]) {
+  public static void main(String[] args) {
     try {
       new CollagerControllerImpl(System.out).run();
     } catch (IOException e) {
