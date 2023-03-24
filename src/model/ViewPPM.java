73e395121b23621ef09c294b5dfa8c9e707852bf
JesseH44 <jdhoogs44@gmail.com>
2023-03-24 15:13:19 -0400

    Added a class to test the FilterImpl

--- src/model/ViewPPM.java
+++ src/model/ViewPPM.java
@@ -4,10 +4,13 @@ import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.io.FileInputStream;
 
+/**
+ * Note: unchanged from supplied class
+ */
 
 /**
- * This class contains utility methods to read a PPM image from file and simply print its contents. Feel free to change this method 
- *  as required.
+ * This class contains utility methods to read a PPM image from file and simply print its contents.
+ * Feel free to change this method as required.
  */
 public class ViewPPM {
 
