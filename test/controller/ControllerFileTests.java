422861362fbc1138fef46c511d0cdb921a2dc1b0
JesseH44 <jdhoogs44@gmail.com>
2023-03-24 14:33:39 -0400

    saveProject test

--- test/controller/ControllerFileTests.java
+++ test/controller/ControllerFileTests.java
@@ -3,6 +3,7 @@ package controller;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -12,6 +13,8 @@ import java.util.Scanner;
 import model.AbstractCollager;
 import model.CollagerPPM;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 /**
@@ -24,6 +27,7 @@ public class ControllerFileTests {
   CollagerController controller;
   Readable in;
   Appendable throwaway = new StringBuilder();
+
   @Before
   public void setUp() {
     collager = new CollagerPPM();
@@ -37,22 +41,42 @@ public class ControllerFileTests {
     try {
       controller.run();
 
-      //tests to see if the project file was properly created
-      Scanner sc = null;
+      //tests to see if the file was created properly
+      File file = new File("C:\\Users\\jdhoo\\Documents\\GitHub\\collager\\Project");
+      assertTrue(file.exists());
+
+      Scanner sc = new Scanner(new FileInputStream
+              ("C:\\Users\\jdhoo\\Documents\\GitHub\\collager\\Project"));
       StringBuilder builder = new StringBuilder();
-      try {
-        sc = new Scanner(new FileInputStream
-                ("C:\\Users\\jdhoo\\Documents\\IntelliJ\\Collager\\Project"));
-      } catch (FileNotFoundException e) {
-        fail("file not created properly");
-      }
-      
       while (sc.hasNextLine()) {
         String s = sc.nextLine();
         builder.append(s + System.lineSeparator());
       }
+      sc = new Scanner(builder.toString());
+
+      //tests that the contents of the file are correct
+      String token = sc.next();
+      int width = sc.nextInt();
+      int height = sc.nextInt();
+      int maxVal = sc.nextInt();
+      String layerName = sc.next();
+      String filterName = sc.next();
+
+      assertEquals("C1", token);
+      assertEquals(5, width);
+      assertEquals(5, height);
+      assertEquals(255, maxVal);
+      assertEquals("background", layerName);
+      assertEquals("normal", filterName);
+      for (int i = 0; i < collager.getHeight(); i++) {
+        for (int k = 0; k < collager.getWidth(); k++) {
+          assertEquals(collager.getLayers().get(0).getLayerImage()[i][k].getRed(), sc.nextInt());
+          assertEquals(collager.getLayers().get(0).getLayerImage()[i][k].getGreen(), sc.nextInt());
+          assertEquals(collager.getLayers().get(0).getLayerImage()[i][k].getBlue(), sc.nextInt());
+        }
+      }
     } catch (IOException e) {
-      //idk
+      throw new IllegalArgumentException("invalid input");
     }
   }
 }
