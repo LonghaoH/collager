b27d0e05801efa674ad06fa14d2a02de0f63e1a2
Nathan Huang <longhaoh1370@gmail.com>
2023-03-25 17:50:10 -0400

    Update CollagerViewImpl.java

--- src/view/CollagerViewImpl.java
+++ src/view/CollagerViewImpl.java
@@ -2,13 +2,7 @@ package view;
 
 import java.awt.BorderLayout;
 
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.*;
 
 /**
  * This class represents an implementation of the Collager view.
@@ -21,8 +15,24 @@ public class CollagerViewImpl extends JFrame implements CollagerView {
   private JMenu edit;
   private JMenuItem fileNewProject;
   private JMenuItem fileOpen;
-  private JMenuItem fileSaveImage;
-  private JMenuItem fileSaveAs;
+  private JMenuItem fileSave;
+  private JMenuItem saveImage;
+  private JMenuItem saveProject;
+  private JMenuItem editAdd;
+  private JMenuItem editSetFilter;
+  private JMenuItem addImage;
+  private JMenuItem addLayer;
+  private JMenuItem normal;
+  private JMenuItem colComponent;
+  private JMenuItem brighten;
+  private JMenuItem darken;
+  private JMenuItem redCom;
+  private JMenuItem greenCom;
+  private JMenuItem blueCom;
+  private JMenuItem value;
+  private JMenuItem intensity;
+  private JMenuItem luma;
+  private JPanel imagePanel;
 
   @Override
   public void createMainFrame() throws UnsupportedLookAndFeelException, ClassNotFoundException,
@@ -49,16 +59,64 @@ public class CollagerViewImpl extends JFrame implements CollagerView {
     //components for file menu
     fileNewProject = new JMenuItem("New Project");
     fileOpen = new JMenuItem("Open");
-    fileSaveImage = new JMenuItem("Save Image");
-    fileSaveAs = new JMenuItem("Save As");
+    fileSave = new JMenuItem("Save...");
     file.add(fileNewProject);
     file.add(fileOpen);
-    file.add(fileSaveImage);
-    file.add(fileSaveAs);
+    file.add(fileSave);
+
+    //components for the save menu item
+    saveImage = new JMenuItem("Save Image");
+    saveProject = new JMenuItem("Save Project");
+    fileSave.add(saveImage);
+    fileSave.add(saveProject);
+
+    //components for edit menu
+    editAdd = new JMenuItem("Add...");
+    editSetFilter = new JMenuItem("Set Filter...");
+    edit.add(editAdd);
+    edit.add(editSetFilter);
+
+    //components for the add menu item
+    addImage = new JMenuItem("Add Image");
+    addLayer = new JMenuItem("Add Layer");
+    editAdd.add(addImage);
+    editAdd.add(addLayer);
+
+    //components for the set filter menu item
+    normal = new JMenuItem("Normal");
+    colComponent = new JMenuItem("Color Component...");
+    brighten = new JMenuItem("Brighten...");
+    darken = new JMenuItem("Darken...");
+    editSetFilter.add(normal);
+    editSetFilter.add(colComponent);
+    editSetFilter.add(brighten);
+    editSetFilter.add(darken);
+
+    //components for the color components menu item
+    redCom = new JMenuItem("Red");
+    greenCom = new JMenuItem("Green");
+    blueCom = new JMenuItem("Blue");
+    colComponent.add(redCom);
+    colComponent.add(greenCom);
+    colComponent.add(blueCom);
+
+    //components for the brighten and darken menu item
+    value = new JMenuItem("Value");
+    intensity = new JMenuItem("Intensity");
+    luma = new JMenuItem("Luma");
+    brighten.add(value);
+    brighten.add(intensity);
+    brighten.add(luma);
+    darken.add(value);
+    darken.add(intensity);
+    darken.add(luma);
   }
 
   @Override
   public void createImagePanel() {
-
+    //panel for displaying the current composite
+    imagePanel = new JPanel();
+    imagePanel.setBorder(BorderFactory.createTitledBorder("Current composite:"));
+    mainPanel.add(imagePanel, BorderLayout.LINE_START);
   }
 }
