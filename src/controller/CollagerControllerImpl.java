b7c0ec34cef1f2788beb63f222f454a30ba8d3a5
JesseH44 <jdhoogs44@gmail.com>
2023-04-05 18:43:08 -0400

    made jar file

--- src/controller/CollagerControllerImpl.java
+++ src/controller/CollagerControllerImpl.java
@@ -253,7 +253,7 @@ public class CollagerControllerImpl implements CollagerController, ActionListene
         }
         currentLayer = currentCollager.getLayers().get(0);
         layers.put("currentLayer", currentLayer);
-        view.updateComposite(currentLayer.getLayerImage());
+        //view.updateComposite(currentLayer.getLayerImage());
       default:
         break;
     }
